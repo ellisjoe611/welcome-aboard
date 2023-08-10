8f5dad5ed8d978c7f9fa80719be721f31c8287bb
ellisjoe611 <ellisjoe@gmail.com>
2023-08-10 10:14:41 +0900

    Modified decorators, initialized mongo documents

--- app/decorators/user.py
+++ app/decorators/user.py
@@ -9,15 +9,7 @@ from app.api import ApiError
 def login_required(func):
     @wraps(func)
     def _wrapper(*args, **kwargs):
-        if "X-Auth-Token" not in request.headers:
-            raise ApiError(message="User login required", status_code=401)
-        try:
-            payload = jwt.decode(request.headers.get("X-Auth-Token"), key=current_app.config["TOKEN_KEY"], algorithms=[current_app.config["ALGORITHM"]])
-        except (DecodeError, InvalidTokenError):
-            raise ApiError(message="Not valid authorization token", status_code=401)
-
-        # payload 로부터 데이터 읽기
-        g.email, g.is_master = payload["email"], payload["is_master"]
+        do_setup_flask_g()
 
         # 지정된 함수 실행하기
         return func(*args, **kwargs)
@@ -25,11 +17,24 @@ def login_required(func):
     return _wrapper
 
 
-def master_only(func):
+def master_login_required(func):
     @wraps(func)
     def _wrapper(*args, **kwargs):
+        do_setup_flask_g()
+
         if g.is_master is not True:
             raise ApiError(message="Not authorized user", status_code=403)
         return func(*args, **kwargs)
 
     return _wrapper
+
+
+def do_setup_flask_g():
+    if "X-Auth-Token" not in request.headers:
+        raise ApiError(message="User login required", status_code=401)
+    try:
+        payload = jwt.decode(request.headers.get("X-Auth-Token"), key=current_app.config["TOKEN_KEY"], algorithms=[current_app.config["ALGORITHM"]])
+    except (DecodeError, InvalidTokenError):
+        raise ApiError(message="Not valid authorization token", status_code=401)
+
+    g.email, g.is_master = payload["email"], payload["is_master"]
